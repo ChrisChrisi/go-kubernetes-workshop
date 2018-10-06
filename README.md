7ee5d44a001ca6efda74fb7859275e9298f99a71
ChristinaMileva <christina.mileva@flyt.io>
2018-10-06 11:47:43 +0300

    adding diagnostics

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # go-kubernetes-workshop
+
+### a repo for the workshop https://github.com/k8s-community/k8s-workshop-eu
--- cmd/go-kubernetes-workshop/main.go
+++ cmd/go-kubernetes-workshop/main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/ChrisChrisi/go-kubernetes-workshop/internal/diagnostics"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	log.Print("Hello, Wold")
-	err := http.ListenAndServe(":8080", nil)
+	log.Print("Hello, World")
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", hello)
+
+	go func() {
+		err := http.ListenAndServe(":8080", router)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	diagnostics := diagnostics.NewDiagnostics()
+	err := http.ListenAndServe(":8585", diagnostics)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, http.StatusText(http.StatusOK))
 }
